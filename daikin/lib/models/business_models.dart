58013eca462d05f2f8c9ce53887d2064d463ce0b
vu111293 <vu111293@mail.com>
2020-01-09 20:26:37 +0700

    add device/room models

---
+++ daikin/lib/models/business_models.dart
@@ -0,0 +1,245 @@
+//"id":1,
+//"name":"zwave",
+//"roomID":0,
+//"type":"com.fibaro.zwavePrimaryController",
+//"baseType":"",
+//"enabled":true,
+//"visible":false,
+//"isPlugin":false,
+//"parentId":0,
+//"remoteGatewayId":0,
+//"viewXml":false,
+//"configXml":false,
+
+import 'package:json_annotation/json_annotation.dart';
+
+part 'business_models.g.dart';
+
+@JsonSerializable(nullable: false)
+class Device {
+  final int id;
+  final String name;
+  final int roomID;
+  final String type;
+  final String baseType;
+  final bool enabled;
+  final bool visible;
+  final bool isPlugin;
+  final int parentId;
+  final int remoteGatewayId;
+  final bool viewXml;
+  final bool configXml;
+  final List<String> interfaces;
+  @JsonKey(toJson: _propertiesToJson)
+  final DeviceProperty properties;
+  @JsonKey(toJson: _actionsToJson)
+  final DeviceAction actions;
+  final int created;
+  final int modified;
+  final int sortOrder;
+
+  Device({this.id, this.name, this.roomID, this.type, this.baseType, this.enabled, this.visible, this.isPlugin, this.parentId, this.remoteGatewayId, this.viewXml, this.configXml, this.interfaces, this.properties, this.actions, this.created, this.modified, this.sortOrder});
+
+  factory Device.fromJson(Map<String, dynamic> json) {
+    final item = _$DeviceFromJson(json);
+    return item;
+  }
+
+  Map<String, dynamic> toJson() => _$DeviceToJson(this);
+}
+
+Map<String, dynamic> _propertiesToJson(DeviceProperty item) => item.toJson();
+
+Map<String, dynamic> _actionsToJson(DeviceAction item) => item.toJson();
+
+@JsonSerializable(nullable: false)
+class DeviceProperty {
+  final String UIMessageSendTime;
+  final String autoConfig;
+  final bool configured;
+  final String date;
+  final String dead;
+  final String deviceControlType;
+  final String deviceIcon;
+  final String disabled;
+  final String emailNotificationID;
+  final String emailNotificationType;
+  final String endPoint;
+  final String endPointId;
+  final String log;
+  final String logTemp;
+  final String manufacturer;
+  final String markAsDead;
+  final String model;
+  final String nodeID;
+  final String nodeId;
+
+  //final String parameters;
+  final String parametersTemplate;
+  final String pollingDeadDevice;
+  final String pollingTime;
+  final String pollingTimeNext;
+  final int pollingTimeSec;
+  final String productInfo;
+  final String pushNotificationID;
+  final String pushNotificationType;
+  final String remoteGatewayId;
+  final String requestNodeNeighborStat;
+  final String requestNodeNeighborStatTimeStemp;
+  final String requestNodeNeighborState;
+  final String requestNodeNeighborStateTimeStemp;
+  final String saveLogs;
+  final String serialNumber;
+  final String showChildren;
+  final String smsNotificationID;
+  final String smsNotificationType;
+  final String status;
+  final String sunriseHour;
+  final String sunsetHour;
+  final String useTemplate;
+  final String itemDescription;
+  final String value;
+  final String zwaveBuildVersion;
+  final String zwaveCompany;
+  final String zwaveInfo;
+  final String zwaveRegion;
+  final String zwaveVersion;
+
+  DeviceProperty(
+      {this.UIMessageSendTime,
+      this.autoConfig,
+      this.configured,
+      this.date,
+      this.dead,
+      this.deviceControlType,
+      this.deviceIcon,
+      this.disabled,
+      this.emailNotificationID,
+      this.emailNotificationType,
+      this.endPoint,
+      this.endPointId,
+      this.log,
+      this.logTemp,
+      this.manufacturer,
+      this.markAsDead,
+      this.model,
+      this.nodeID,
+      this.nodeId,
+      //this.parameters,
+      this.parametersTemplate,
+      this.pollingDeadDevice,
+      this.pollingTime,
+      this.pollingTimeNext,
+      this.pollingTimeSec,
+      this.productInfo,
+      this.pushNotificationID,
+      this.pushNotificationType,
+      this.remoteGatewayId,
+      this.requestNodeNeighborStat,
+      this.requestNodeNeighborStatTimeStemp,
+      this.requestNodeNeighborState,
+      this.requestNodeNeighborStateTimeStemp,
+      this.saveLogs,
+      this.serialNumber,
+      this.showChildren,
+      this.smsNotificationID,
+      this.smsNotificationType,
+      this.status,
+      this.sunriseHour,
+      this.sunsetHour,
+      this.useTemplate,
+      this.itemDescription,
+      this.value,
+      this.zwaveBuildVersion,
+      this.zwaveCompany,
+      this.zwaveInfo,
+      this.zwaveRegion,
+      this.zwaveVersion});
+
+  factory DeviceProperty.fromJson(Map<String, dynamic> json) {
+    final item = _$DevicePropertyFromJson(json);
+    return item;
+  }
+
+  Map<String, dynamic> toJson() => _$DevicePropertyToJson(this);
+}
+
+//"pollingDeadDevice":1,
+//"pollingTimeSec":1,
+//"reconfigure":0,
+//"requestNodeNeighborUpdate":1,
+//"turnOff":0,
+//"turnOn":0
+@JsonSerializable(nullable: false)
+class DeviceAction {
+  final int pollingDeadDevice;
+  final int pollingTimeSec;
+  final int reconfigure;
+  final int requestNodeNeighborUpdate;
+  final int turnOff;
+  final int turnOn;
+
+  DeviceAction({this.pollingDeadDevice, this.pollingTimeSec, this.reconfigure, this.requestNodeNeighborUpdate, this.turnOff, this.turnOn});
+
+  factory DeviceAction.fromJson(Map<String, dynamic> json) {
+    final item = _$DeviceActionFromJson(json);
+    return item;
+  }
+
+  Map<String, dynamic> toJson() => _$DeviceActionToJson(this);
+}
+
+//{
+//"id":18,
+//"name":"Tiếp Tân",
+//"sectionID":18,
+//"icon":"room_polkabuty",
+//"defaultSensors":{
+//"temperature":192,
+//"humidity":0,
+//"light":193
+//},
+//"defaultThermostat":0,
+//"sortOrder":2,
+//"category":"other"
+//},
+
+@JsonSerializable(nullable: false)
+class Room {
+  final int id;
+  final String name;
+  final int sectionID;
+  final String icon;
+  final int defaultThermostat;
+  final int sortOrder;
+  final int category;
+  @JsonKey(toJson: _defSensorToJson)
+  final RoomDefaultSensor defaultSensors;
+
+  Room({this.id, this.name, this.sectionID, this.icon, this.defaultThermostat, this.defaultSensors, this.sortOrder, this.category});
+
+  factory Room.fromJson(Map<String, dynamic> json) {
+    final item = _$RoomFromJson(json);
+    return item;
+  }
+
+  Map<String, dynamic> toJson() => _$RoomToJson(this);
+}
+
+Map<String, dynamic> _defSensorToJson(RoomDefaultSensor item) => item.toJson();
+
+@JsonSerializable(nullable: false)
+class RoomDefaultSensor {
+  final int temperature;
+  final int humidity;
+  final int light;
+
+  RoomDefaultSensor({this.temperature, this.humidity, this.light});
+
+  factory RoomDefaultSensor.fromJson(Map<String, dynamic> json) {
+    final item = _$RoomDefaultSensorFromJson(json);
+    return item;
+  }
+
+  Map<String, dynamic> toJson() => _$RoomDefaultSensorToJson(this);
+}
---
+++ daikin/lib/models/business_models.g.dart
@@ -0,0 +1,222 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'business_models.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Device _$DeviceFromJson(Map<String, dynamic> json) {
+  return Device(
+    id: json['id'] as int,
+    name: json['name'] as String,
+    roomID: json['roomID'] as int,
+    type: json['type'] as String,
+    baseType: json['baseType'] as String,
+    enabled: json['enabled'] as bool,
+    visible: json['visible'] as bool,
+    isPlugin: json['isPlugin'] as bool,
+    parentId: json['parentId'] as int,
+    remoteGatewayId: json['remoteGatewayId'] as int,
+    viewXml: json['viewXml'] as bool,
+    configXml: json['configXml'] as bool,
+    interfaces: (json['interfaces'] as List).map((e) => e as String).toList(),
+    properties:
+        DeviceProperty.fromJson(json['properties'] as Map<String, dynamic>),
+    actions: DeviceAction.fromJson(json['actions'] as Map<String, dynamic>),
+    created: json['created'] as int,
+    modified: json['modified'] as int,
+    sortOrder: json['sortOrder'] as int,
+  );
+}
+
+Map<String, dynamic> _$DeviceToJson(Device instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'roomID': instance.roomID,
+      'type': instance.type,
+      'baseType': instance.baseType,
+      'enabled': instance.enabled,
+      'visible': instance.visible,
+      'isPlugin': instance.isPlugin,
+      'parentId': instance.parentId,
+      'remoteGatewayId': instance.remoteGatewayId,
+      'viewXml': instance.viewXml,
+      'configXml': instance.configXml,
+      'interfaces': instance.interfaces,
+      'properties': _propertiesToJson(instance.properties),
+      'actions': _actionsToJson(instance.actions),
+      'created': instance.created,
+      'modified': instance.modified,
+      'sortOrder': instance.sortOrder,
+    };
+
+DeviceProperty _$DevicePropertyFromJson(Map<String, dynamic> json) {
+  return DeviceProperty(
+    UIMessageSendTime: json['UIMessageSendTime'] as String,
+    autoConfig: json['autoConfig'] as String,
+    configured: json['configured'] as bool,
+    date: json['date'] as String,
+    dead: json['dead'] as String,
+    deviceControlType: json['deviceControlType'] as String,
+    deviceIcon: json['deviceIcon'] as String,
+    disabled: json['disabled'] as String,
+    emailNotificationID: json['emailNotificationID'] as String,
+    emailNotificationType: json['emailNotificationType'] as String,
+    endPoint: json['endPoint'] as String,
+    endPointId: json['endPointId'] as String,
+    log: json['log'] as String,
+    logTemp: json['logTemp'] as String,
+    manufacturer: json['manufacturer'] as String,
+    markAsDead: json['markAsDead'] as String,
+    model: json['model'] as String,
+    nodeID: json['nodeID'] as String,
+    nodeId: json['nodeId'] as String,
+    parametersTemplate: json['parametersTemplate'] as String,
+    pollingDeadDevice: json['pollingDeadDevice'] as String,
+    pollingTime: json['pollingTime'] as String,
+    pollingTimeNext: json['pollingTimeNext'] as String,
+    pollingTimeSec: json['pollingTimeSec'] as int,
+    productInfo: json['productInfo'] as String,
+    pushNotificationID: json['pushNotificationID'] as String,
+    pushNotificationType: json['pushNotificationType'] as String,
+    remoteGatewayId: json['remoteGatewayId'] as String,
+    requestNodeNeighborStat: json['requestNodeNeighborStat'] as String,
+    requestNodeNeighborStatTimeStemp:
+        json['requestNodeNeighborStatTimeStemp'] as String,
+    requestNodeNeighborState: json['requestNodeNeighborState'] as String,
+    requestNodeNeighborStateTimeStemp:
+        json['requestNodeNeighborStateTimeStemp'] as String,
+    saveLogs: json['saveLogs'] as String,
+    serialNumber: json['serialNumber'] as String,
+    showChildren: json['showChildren'] as String,
+    smsNotificationID: json['smsNotificationID'] as String,
+    smsNotificationType: json['smsNotificationType'] as String,
+    status: json['status'] as String,
+    sunriseHour: json['sunriseHour'] as String,
+    sunsetHour: json['sunsetHour'] as String,
+    useTemplate: json['useTemplate'] as String,
+    itemDescription: json['itemDescription'] as String,
+    value: json['value'] as String,
+    zwaveBuildVersion: json['zwaveBuildVersion'] as String,
+    zwaveCompany: json['zwaveCompany'] as String,
+    zwaveInfo: json['zwaveInfo'] as String,
+    zwaveRegion: json['zwaveRegion'] as String,
+    zwaveVersion: json['zwaveVersion'] as String,
+  );
+}
+
+Map<String, dynamic> _$DevicePropertyToJson(DeviceProperty instance) =>
+    <String, dynamic>{
+      'UIMessageSendTime': instance.UIMessageSendTime,
+      'autoConfig': instance.autoConfig,
+      'configured': instance.configured,
+      'date': instance.date,
+      'dead': instance.dead,
+      'deviceControlType': instance.deviceControlType,
+      'deviceIcon': instance.deviceIcon,
+      'disabled': instance.disabled,
+      'emailNotificationID': instance.emailNotificationID,
+      'emailNotificationType': instance.emailNotificationType,
+      'endPoint': instance.endPoint,
+      'endPointId': instance.endPointId,
+      'log': instance.log,
+      'logTemp': instance.logTemp,
+      'manufacturer': instance.manufacturer,
+      'markAsDead': instance.markAsDead,
+      'model': instance.model,
+      'nodeID': instance.nodeID,
+      'nodeId': instance.nodeId,
+      'parametersTemplate': instance.parametersTemplate,
+      'pollingDeadDevice': instance.pollingDeadDevice,
+      'pollingTime': instance.pollingTime,
+      'pollingTimeNext': instance.pollingTimeNext,
+      'pollingTimeSec': instance.pollingTimeSec,
+      'productInfo': instance.productInfo,
+      'pushNotificationID': instance.pushNotificationID,
+      'pushNotificationType': instance.pushNotificationType,
+      'remoteGatewayId': instance.remoteGatewayId,
+      'requestNodeNeighborStat': instance.requestNodeNeighborStat,
+      'requestNodeNeighborStatTimeStemp':
+          instance.requestNodeNeighborStatTimeStemp,
+      'requestNodeNeighborState': instance.requestNodeNeighborState,
+      'requestNodeNeighborStateTimeStemp':
+          instance.requestNodeNeighborStateTimeStemp,
+      'saveLogs': instance.saveLogs,
+      'serialNumber': instance.serialNumber,
+      'showChildren': instance.showChildren,
+      'smsNotificationID': instance.smsNotificationID,
+      'smsNotificationType': instance.smsNotificationType,
+      'status': instance.status,
+      'sunriseHour': instance.sunriseHour,
+      'sunsetHour': instance.sunsetHour,
+      'useTemplate': instance.useTemplate,
+      'itemDescription': instance.itemDescription,
+      'value': instance.value,
+      'zwaveBuildVersion': instance.zwaveBuildVersion,
+      'zwaveCompany': instance.zwaveCompany,
+      'zwaveInfo': instance.zwaveInfo,
+      'zwaveRegion': instance.zwaveRegion,
+      'zwaveVersion': instance.zwaveVersion,
+    };
+
+DeviceAction _$DeviceActionFromJson(Map<String, dynamic> json) {
+  return DeviceAction(
+    pollingDeadDevice: json['pollingDeadDevice'] as int,
+    pollingTimeSec: json['pollingTimeSec'] as int,
+    reconfigure: json['reconfigure'] as int,
+    requestNodeNeighborUpdate: json['requestNodeNeighborUpdate'] as int,
+    turnOff: json['turnOff'] as int,
+    turnOn: json['turnOn'] as int,
+  );
+}
+
+Map<String, dynamic> _$DeviceActionToJson(DeviceAction instance) =>
+    <String, dynamic>{
+      'pollingDeadDevice': instance.pollingDeadDevice,
+      'pollingTimeSec': instance.pollingTimeSec,
+      'reconfigure': instance.reconfigure,
+      'requestNodeNeighborUpdate': instance.requestNodeNeighborUpdate,
+      'turnOff': instance.turnOff,
+      'turnOn': instance.turnOn,
+    };
+
+Room _$RoomFromJson(Map<String, dynamic> json) {
+  return Room(
+    id: json['id'] as int,
+    name: json['name'] as String,
+    sectionID: json['sectionID'] as int,
+    icon: json['icon'] as String,
+    defaultThermostat: json['defaultThermostat'] as int,
+    defaultSensors: RoomDefaultSensor.fromJson(
+        json['defaultSensors'] as Map<String, dynamic>),
+    sortOrder: json['sortOrder'] as int,
+    category: json['category'] as int,
+  );
+}
+
+Map<String, dynamic> _$RoomToJson(Room instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'sectionID': instance.sectionID,
+      'icon': instance.icon,
+      'defaultThermostat': instance.defaultThermostat,
+      'sortOrder': instance.sortOrder,
+      'category': instance.category,
+      'defaultSensors': _defSensorToJson(instance.defaultSensors),
+    };
+
+RoomDefaultSensor _$RoomDefaultSensorFromJson(Map<String, dynamic> json) {
+  return RoomDefaultSensor(
+    temperature: json['temperature'] as int,
+    humidity: json['humidity'] as int,
+    light: json['light'] as int,
+  );
+}
+
+Map<String, dynamic> _$RoomDefaultSensorToJson(RoomDefaultSensor instance) =>
+    <String, dynamic>{
+      'temperature': instance.temperature,
+      'humidity': instance.humidity,
+      'light': instance.light,
+    };
