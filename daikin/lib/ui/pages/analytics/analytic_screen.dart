e3e44c161055cc59e35507d6ad2adcbf4f0f9982
vu111293 <vu111293@mail.com>
2020-01-19 06:54:39 +0700

    camera test

--- daikin/lib/ui/pages/analytics/analytic_screen.dart
+++ daikin/lib/ui/pages/analytics/analytic_screen.dart
@@ -7,6 +7,7 @@ import 'package:daikin/constants/styleAppTheme.dart';
 import 'package:daikin/ui/route/route/routing.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class AnalyticScreen extends StatefulWidget {
@@ -24,6 +25,61 @@ class AnalyticScreenState extends State<AnalyticScreen> with SingleTickerProvide
     _appBloc = BlocProvider.of<ApplicationBloc>(context);
   }
 
+  Widget _basicWv() {
+    return WebView(
+      initialUrl: 'http://admin:admin123@mhome-showroom.ddns.net:20031/Streaming/channels/2/preview',
+      javascriptMode: JavascriptMode.unrestricted,
+      debuggingEnabled: true,
+      userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+      onWebViewCreated: (WebViewController webViewController) {
+        _controller.complete(webViewController);
+        webViewController.evaluateJavascript('javascript:document.getElementsByTagName(\'img\')[0].style.width=\'100%\';');
+      },
+      onPageFinished: (s) {
+        print(s);
+      },
+    );
+  }
+
+  InAppWebViewController webView;
+  Widget _buildWv() {
+    return InAppWebView(
+      initialUrl: "http://admin:admin123@mhome-showroom.ddns.net:20033/Streaming/channels/2/preview",
+      initialHeaders: {},
+      initialOptions: InAppWebViewWidgetOptions(
+        android: AndroidInAppWebViewOptions(
+          builtInZoomControls: true,
+          loadWithOverviewMode: true,
+          useWideViewPort: true
+        )
+//          inAppWebViewOptions: InAppWebViewOptions(
+//            debuggingEnabled: true,
+//          ),
+//          androidInAppWebViewOptions: AndroidInAppWebViewOptions(
+//              loadWithOverviewMode: true
+//          )
+      ),
+      onWebViewCreated: (InAppWebViewController controller) {
+        webView = controller;
+      },
+      onLoadStart: (InAppWebViewController controller, String url) {
+        setState(() {
+//          this.url = url;
+        });
+      },
+      onLoadStop: (InAppWebViewController controller, String url) async {
+        setState(() {
+//          this.url = url;
+        });
+      },
+      onProgressChanged: (InAppWebViewController controller, int progress) {
+        setState(() {
+//          this.progress = progress / 100;
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Routing().setContext(context);
@@ -35,19 +91,7 @@ class AnalyticScreenState extends State<AnalyticScreen> with SingleTickerProvide
           body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[Container(alignment: Alignment.center, child: Text('Analytics page')),
-              Container(width: 300, height: 300, child: WebView(
-                initialUrl: 'http://admin:admin123@mhome-showroom.ddns.net:20031/Streaming/channels/2/preview',
-                javascriptMode: JavascriptMode.unrestricted,
-                debuggingEnabled: true,
-                userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
-                onWebViewCreated: (WebViewController webViewController) {
-                  _controller.complete(webViewController);
-                  webViewController.evaluateJavascript('javascript:document.getElementsByTagName(\'img\')[0].style.width=\'100%\';');
-                },
-                onPageFinished: (s) {
-                  print(s);
-                },
-              )),
+              Container(width: 300, height: 300, child: _buildWv()),
               Expanded(child: Container(
                   color: Colors.white))
             ],
