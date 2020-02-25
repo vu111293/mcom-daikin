ead6f41736c2d89af810c22faaf163a2fafe3b68
Đâu Phải Phát <phat.tran1201@gmail.com>
2020-02-25 21:29:53 +0700

    9. Danh sách Phòng - Thiết bị không mở --> bỏ viền xanh nhạt

--- daikin/lib/ui/pages/device_detail/buid_device_item.dart
+++ daikin/lib/ui/pages/device_detail/buid_device_item.dart
@@ -26,7 +26,7 @@ Widget defaultBuildDevice(DeviceViewItem widget, Device device, bool isSwitched,
             shadowColor: Colors.black26,
             color: isSwitched ? Colors.white : HexColor("#f3f3f3"),
             shape: RoundedRectangleBorder(
-              side: BorderSide(color: HexColor(appBorderColor2).withOpacity(0.1), width: 1),
+              // side: BorderSide(color: HexColor(appBorderColor2).withOpacity(0.1), width: 1),
               borderRadius: BorderRadius.all(Radius.circular(10)),
             ),
             child: InkWell(
@@ -131,8 +131,7 @@ Widget buildBlindsDevice(
             shadowColor: Colors.black26,
             color: isSwitched ? Colors.white : HexColor("#f3f3f3"),
             shape: RoundedRectangleBorder(
-              side: BorderSide(
-                  color: HexColor(appBorderColor2).withOpacity(0.1), width: 1),
+              // side: BorderSide(color: HexColor(appBorderColor2).withOpacity(0.1), width: 1),
               borderRadius: BorderRadius.all(Radius.circular(10)),
             ),
             child: InkWell(
@@ -215,7 +214,7 @@ Widget buildSwitchDevice(DeviceViewItem widget, Device device, Function callback
             shadowColor: Colors.black26,
             color: isSwitched ? Colors.white : HexColor("#f3f3f3"),
             shape: RoundedRectangleBorder(
-              side: BorderSide(color: HexColor(appBorderColor2).withOpacity(0.1), width: 1),
+              // side: BorderSide(color: HexColor(appBorderColor2).withOpacity(0.1), width: 1),
               borderRadius: BorderRadius.all(Radius.circular(10)),
             ),
             child: InkWell(
@@ -320,7 +319,7 @@ Widget buildRGBDevice(DeviceViewItem widget, Device device, Function callback, F
             shadowColor: Colors.black26,
             color: isSwitched ? Colors.white : HexColor("#f3f3f3"),
             shape: RoundedRectangleBorder(
-              side: BorderSide(color: HexColor(appBorderColor2).withOpacity(0.1), width: 1),
+              // side: BorderSide(color: HexColor(appBorderColor2).withOpacity(0.1), width: 1),
               borderRadius: BorderRadius.all(Radius.circular(10)),
             ),
             child: InkWell(
@@ -429,7 +428,7 @@ Widget buildVirtualDevice(DeviceViewItem widget, Device device) {
             shadowColor: Colors.black26,
             color: HexColor("#f3f3f3"),
             shape: RoundedRectangleBorder(
-              side: BorderSide(color: HexColor(appBorderColor2).withOpacity(0.1), width: 1),
+              // side: BorderSide(color: HexColor(appBorderColor2).withOpacity(0.1), width: 1),
               borderRadius: BorderRadius.all(Radius.circular(10)),
             ),
             child: InkWell(
@@ -525,7 +524,7 @@ Widget buildSwitchMultiDevice(DeviceViewItem widget, Device device,
             shadowColor: Colors.black26,
             color: isSwitched ? Colors.white : HexColor("#f3f3f3"),
             shape: RoundedRectangleBorder(
-              side: BorderSide(color: HexColor(appBorderColor2).withOpacity(0.1), width: 1),
+              // side: BorderSide(color: HexColor(appBorderColor2).withOpacity(0.1), width: 1),
               borderRadius: BorderRadius.all(Radius.circular(10)),
             ),
             child: InkWell(
