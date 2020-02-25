cb9352eb0d8ba5b2a9aee48624fc456a1a74f6e4
Đâu Phải Phát <phat.tran1201@gmail.com>
2020-02-25 23:31:30 +0700

    Bị mất tính năng click vào thiết bị không chuyển đến trang chi tiết của thiết bị

--- daikin/lib/ui/pages/dashboard/active_device_view.dart
+++ daikin/lib/ui/pages/dashboard/active_device_view.dart
@@ -7,13 +7,18 @@ import 'package:daikin/constants/dataTest.dart';
 import 'package:daikin/constants/styleAppTheme.dart';
 import 'package:daikin/models/business_models.dart';
 import 'package:daikin/ui/pages/dashboard/rgb_screen.dart';
+import 'package:daikin/ui/pages/device_detail/blinds_device_screen.dart';
+import 'package:daikin/ui/pages/device_detail/device_on_off_detail_screen.dart';
 import 'package:daikin/ui/pages/device_detail/device_view_item.dart';
+import 'package:daikin/ui/pages/device_detail/switch_multi_device.dart';
+import 'package:daikin/ui/pages/device_detail/virtual_device_screen.dart';
 import 'package:daikin/ui/route/route/routing.dart';
 import 'package:daikin/utils/hex_color.dart';
 import 'package:flutter/material.dart';
 
 class ActiveDeviceListView extends StatefulWidget {
-  const ActiveDeviceListView({Key key, this.callBack}) : super(key: key);
+  const ActiveDeviceListView({Key key, this.callBack})
+      : super(key: key);
 
   final Function callBack;
   @override
@@ -80,6 +85,9 @@ class _ActiveDeviceListViewState extends State<ActiveDeviceListView>
 
                     return DeviceView(
                       device: snapshot.data[index],
+                      callbackParent: () {
+                        routingToDevicePage(snapshot.data[index]);
+                      },
                       callback: (value) {
                         BusinessService()
                             .turnOffDevice(snapshot.data[index].id);
@@ -99,129 +107,255 @@ class _ActiveDeviceListViewState extends State<ActiveDeviceListView>
       ),
     );
   }
+
+  onSwitchDevice(bool val, Device device) {
+    device.properties.value = val.toString();
+    //BotToast.showText(text: 'Đổi sang trạng thái ' + device.properties.value);
+    setState(() {});
+
+    if (!val) {
+      BusinessService().turnOffDevice(device.id);
+      BotToast.showText(text: "Tắt thiết bị thành công");
+    } else {
+      BusinessService().turnOnDevice(device.id);
+      BotToast.showText(text: "Bật thiết bị thành công");
+    }
+    _appBloc.homeBloc.updateActiveDevice();
+  }
+
+  void _refreshDeviceInfo(Device d) async {
+    Device device = await BusinessService().getDeviceDetail(d.id);
+    _appBloc.homeBloc.updateDevice(device);
+    setState(() {
+      d = device;
+    });
+  }
+
+  onSwitchMultiDevice(int val, Device device) {
+    //BotToast.showText(text: 'Đổi sang trạng thái ' + device.properties.value);
+    setState(() {
+      device.properties.value = val.toString();
+    });
+
+    if (val == 0) {
+      BusinessService().setValue(device.id, val);
+      BotToast.showText(text: "Tắt thiết bị thành công");
+    } else {
+      BusinessService().setValue(device.id, val);
+      BotToast.showText(text: "Thay đổi độ sáng thành công");
+    }
+
+    // if (!val) {
+    //   BusinessService().turnOffDevice(device.id);
+    //   BotToast.showText(text: "Tắt thiết bị thành công");
+    // } else {
+    //   BusinessService().turnOnDevice(device.id);
+    //   BotToast.showText(text: "Bật thiết bị thành công");
+    // }
+  }
+
+  onClickMultiDevice(bool val, Device device) {
+    if (!val) {
+      setState(() {
+        device.properties.value = 0.toString();
+      });
+
+      BusinessService().turnOffDevice(device.id);
+      BotToast.showText(text: "Tắt thiết bị thành công");
+    } else {
+      setState(() {
+        device.properties.value = 99.toString();
+      });
+
+      BusinessService().turnOnDevice(device.id);
+      BotToast.showText(text: "Bật thiết bị thành công");
+    }
+    _appBloc.homeBloc.updateActiveDevice();
+  }
+
+  void routingToDevicePage(Device device) {
+    if (device.type == "com.fibaro.binarySwitch") {
+      bool isSwitched = device.properties.value == 'true' ? true : false;
+
+      Routing().navigate2(
+          context,
+          DeviceOnOffDetailScreen(
+            item: device,
+            status: isSwitched,
+            callback: (value) {
+              onSwitchDevice(value, device);
+            },
+          ));
+    } else if (device.type == "com.fibaro.FGRGBW441M") {
+      Routing().navigate2(context, RgbScreen(device: device)).then((d) {
+        _refreshDeviceInfo(device);
+      });
+    } else if (device.type == "com.fibaro.FGD212") {
+      Routing().navigate2(
+          context,
+          SwitchMultiDevice(
+            device: device,
+            setValue: (value) {
+              onSwitchMultiDevice(value, device);
+            },
+            onClick: (value) {
+              onClickMultiDevice(value, device);
+            },
+          ));
+    } else if (device.type == "virtual_device") {
+      Routing().navigate2(context, VirtualDeviceScreen(device: device));
+    } else if (device.type == "com.fibaro.FGRM222") {
+      Routing().navigate2(
+          context,
+          BlindsDeviceScreen(
+            device: device,
+          ));
+    } else {
+      bool isSwitched = device.properties.value == 'true' ? true : false;
+
+      Routing().navigate2(
+          context,
+          DeviceOnOffDetailScreen(
+            item: device,
+            status: isSwitched,
+            callback: () {},
+          ));
+    }
+  }
 }
 
 class DeviceView extends StatelessWidget {
-  const DeviceView({Key key, this.device, this.callback}) : super(key: key);
+  const DeviceView({Key key, this.device, this.callback, this.callbackParent})
+      : super(key: key);
 
   final Function callback;
+  final Function callbackParent;
   final Device device;
   final bool isSwitched = true;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 280,
-      child: Stack(
-        children: <Widget>[
-          Container(
-            child: Row(
-              children: <Widget>[
-                const SizedBox(
-                  width: 48,
-                ),
-                Expanded(
+    return InkWell(
+      splashColor: Colors.transparent,
+      onTap: () {
+        callbackParent();
+      },
+      child: SizedBox(
+        width: 280,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              child: Row(
+                children: <Widget>[
+                  const SizedBox(
+                    width: 48,
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: HexColor('#F8FAFB'),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(16.0)),
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          const SizedBox(
+                            width: 48 + 24.0,
+                          ),
+                          Expanded(
+                            child: Container(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 16),
+                                    child: Text(
+                                      device.name,
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 16,
+                                        letterSpacing: 0.27,
+                                        color: StyleAppTheme.darkerText,
+                                      ),
+                                    ),
+                                  ),
+                                  // const Expanded(
+                                  //   child: SizedBox(),
+                                  // ),
+                                  Container(
+                                      height: 30,
+                                      child: Stack(
+                                        children: <Widget>[
+                                          Positioned(
+                                            top: -5,
+                                            left: -10,
+                                            child: Transform.scale(
+                                              scale: 1.0,
+                                              child: Switch(
+                                                value: isSwitched,
+                                                onChanged: (value) {
+                                                  callback(value);
+                                                  //callback(value);
+                                                  // setState(() {
+                                                  //   isSwitched = value;
+                                                  // });
+                                                  BusinessService()
+                                                      .turnOffDevice(device.id);
+                                                  BotToast.showText(
+                                                      text:
+                                                          "Tắt thiết bị thành công");
+                                                },
+                                                materialTapTargetSize:
+                                                    MaterialTapTargetSize
+                                                        .padded,
+                                                activeColor: Colors.white,
+                                                activeTrackColor:
+                                                    HexColor(appColor),
+                                                inactiveThumbColor:
+                                                    HexColor(appBorderColor),
+                                                inactiveTrackColor:
+                                                    HexColor(appBorderColor),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      )),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              child: Padding(
+                  padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
                   child: Container(
+                    padding: EdgeInsets.all(10),
                     decoration: BoxDecoration(
-                      color: HexColor('#F8FAFB'),
+                      color: Colors.white,
                       borderRadius:
                           const BorderRadius.all(Radius.circular(16.0)),
                     ),
-                    child: Row(
-                      children: <Widget>[
-                        const SizedBox(
-                          width: 48 + 24.0,
-                        ),
-                        Expanded(
-                          child: Container(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 16),
-                                  child: Text(
-                                    device.name,
-                                    textAlign: TextAlign.left,
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 16,
-                                      letterSpacing: 0.27,
-                                      color: StyleAppTheme.darkerText,
-                                    ),
-                                  ),
-                                ),
-                                // const Expanded(
-                                //   child: SizedBox(),
-                                // ),
-                                Container(
-                                    height: 30,
-                                    child: Stack(
-                                      children: <Widget>[
-                                        Positioned(
-                                          top: -5,
-                                          left: -10,
-                                          child: Transform.scale(
-                                            scale: 1.0,
-                                            child: Switch(
-                                              value: isSwitched,
-                                              onChanged: (value) {
-                                                callback(value);
-                                                //callback(value);
-                                                // setState(() {
-                                                //   isSwitched = value;
-                                                // });
-                                                BusinessService()
-                                                    .turnOffDevice(device.id);
-                                                BotToast.showText(
-                                                    text:
-                                                        "Tắt thiết bị thành công");
-                                              },
-                                              materialTapTargetSize:
-                                                  MaterialTapTargetSize.padded,
-                                              activeColor: Colors.white,
-                                              activeTrackColor:
-                                                  HexColor(appColor),
-                                              inactiveThumbColor:
-                                                  HexColor(appBorderColor),
-                                              inactiveTrackColor:
-                                                  HexColor(appBorderColor),
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    )),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
+                    child: ClipRRect(
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(16.0)),
+                      child: AspectRatio(
+                          aspectRatio: 0.9,
+                          child: Image.asset(
+                            'assets/devices/rgb.png',
+                          )),
                     ),
-                  ),
-                )
-              ],
+                  )),
             ),
-          ),
-          Container(
-            child: Padding(
-                padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-                child: Container(
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: const BorderRadius.all(Radius.circular(16.0)),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: const BorderRadius.all(Radius.circular(16.0)),
-                    child: AspectRatio(
-                        aspectRatio: 0.9,
-                        child: Image.asset(
-                          'assets/devices/rgb.png',
-                        )),
-                  ),
-                )),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
