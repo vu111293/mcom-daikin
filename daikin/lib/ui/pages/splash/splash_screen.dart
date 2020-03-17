0f59afcc7b6cc2613a45377ff267aeb623a987de
vu111293 <vu111293@mail.com>
2020-03-17 23:11:46 +0700

    fix timeout

--- daikin/lib/ui/pages/splash/splash_screen.dart
+++ daikin/lib/ui/pages/splash/splash_screen.dart
@@ -73,7 +73,7 @@ class _SplashScreenState extends State<SplashScreen> {
             if (tokenState != AccessStatus.TOKEN_VALID) {
               _openLoginScreen();
             } else {
-              LUser user = await UserService().login(LoopBackAuth().fbToken);
+              LUser user = await UserService().me();
               _appBloc.authBloc.updateUserAction(user);
               Routing().navigate2(context, MainScreen());
               _setupStateStream.cancel();
