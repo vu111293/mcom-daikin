f7a3e58add779ec918bc06969b6223d3aadef7f4
vule <vu111293@gmail.com>
2020-06-17 18:25:57 +0700

    up miss file

--- daikin/lib/utils/formatTextFirstUpCase.dart
+++ daikin/lib/utils/formatTextFirstUpCase.dart
@@ -1,7 +1,17 @@
+
 String upFirstText(String text) {
   if (text != null && text.length > 1) {
     return text[0].toUpperCase() + text?.substring(1)?.toLowerCase();
   } else {
     return text != null ? text?.toUpperCase() : '';
   }
+}
+
+String uppercaseFirstInWord(String text) {
+  String ret = text.trim().split(' ')
+      .map((item) {
+        return item.trim().isNotEmpty ? '${item[0].toUpperCase()}${item.substring(1)}' : ''; })
+      .toList()
+      .join(' ');
+  return ret;
 }
